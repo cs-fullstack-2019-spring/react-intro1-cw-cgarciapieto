93c65b14f5380647e1e12cf78f95dab3725f3f6c
carlos <cgarciapieto@gmail.com>
2019-03-29 15:19:17 -0500

    update

--- got_bank/src/App.js
+++ got_bank/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BankAccount/>
+        <BankAccount balance = "$2112" name = 'Carlos'/>
       </div>
     );
   }
--- got_bank/src/BankAccount.js
+++ got_bank/src/BankAccount.js
@@ -3,16 +3,15 @@ import './App.css';
 
 //creates two variables and passes into the BankAccount component and exports 
 
-const name = 'Carlos';
-const balance = "$2112";
+
 class BankAccount extends Component{
     render(){
         return(
             
             <div>
 
-            <h2> Welcome { name }</h2>
-            <h3>....and your balance is {balance}</h3>    
+            <h2> Welcome {this.props.name}</h2>
+            <h3>....and your balance is {this.props.balance}</h3>
             </div>
             
         );
